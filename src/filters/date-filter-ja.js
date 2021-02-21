72486ec9e1e3355c6e5340ffdb19a364af28c132
kawakami <saruyama.monki@gmail.com>
2021-02-21 23:47:31 +0900

    modify date

--- src/filters/date-filter-ja.js
+++ src/filters/date-filter-ja.js
@@ -1,5 +1,5 @@
 module.exports = function dateFilterJa(value) {
   const dateObject = new Date(value);
 
-  return `${dateObject.getFullYear()}-${("0" + dateObject.getMonth()).slice(-2)}-${("0" + dateObject.getDate()).slice(-2)}`;
+  return `${dateObject.getFullYear()}-${("0" + (dateObject.getMonth() + 1)).slice(-2)}-${("0" + dateObject.getDate()).slice(-2)}`;
 }
